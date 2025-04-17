3227f00a66f02463b1c22286b770eb1335233d1f
tonychg <tonychg7@pm.me>
2025-04-17 15:06:52 +0200

    feat: remove extra module for yaml schemas

--- lsp/yaml.lua
+++ lsp/yaml.lua
@@ -8,7 +8,17 @@ return {
       validate = true,
       hover = true,
       trace = { server = "debug" },
-      schemas = require("config.yaml-schemas"),
+      schemas = {
+        ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = {
+          "ci/*.{yaml,yml}",
+          ".gitlab/**/*.{yaml,yml}",
+          ".gitlab-ci.{yaml,yml}",
+        },
+        ["https://taskfile.dev/schema.json"] = {
+          "**/Taskfile*.{yaml,yml}",
+          "taskfiles/**/*.{yaml,yml}",
+        },
+      },
     },
   },
 }
