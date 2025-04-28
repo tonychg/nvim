06300ddf7099c27abecf675f9ba5a0dfadcf38fb
tonychg <tonychg7@pm.me>
2025-04-28 14:14:00 +0200

    feat(fzf): add keymap for search all files in ~/ghq

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -3,30 +3,20 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
     files = {
-      fd_opts =
-      [[--color=never --type f --hidden --follow --exclude .git --exclude *.cur --exclude *.mp4 --exclude *.gif --exclude *.png --exclude *.jpg --exclude *.PNG --exclude *.mp3]],
+      fd_opts = [[--color=never --type f --hidden --follow --exclude .git --exclude *.cur --exclude *.mp4 --exclude *.gif --exclude *.png --exclude *.jpg --exclude *.PNG --exclude *.mp3]],
     },
   },
   config = function()
     -- fzf
     require("which-key").add({
-      { "<leader>f", group = "🔍Search" }
+      { "<leader>f", group = "🔍Search" },
     })
-    vim.keymap.set("n", "<leader>ff",
-      require('fzf-lua').files,
-      { silent = true, desc = "🔍Search files" }
-    )
-    vim.keymap.set("n", "<leader>x",
-      require('fzf-lua').quickfix,
-      { silent = true, desc = "🔍Open quickfix" }
-    )
-    vim.keymap.set("n", "<leader>fg",
-      require('fzf-lua').live_grep,
-      { silent = true, desc = "🔍Search live grep" }
-    )
-    vim.keymap.set("n", "<leader>fb",
-      require('fzf-lua').buffers,
-      { silent = true, desc = "🔍Search buffers" }
-    )
-  end
+    vim.keymap.set("n", "<leader>ff", require("fzf-lua").files, { silent = true, desc = "🔍Search files" })
+    vim.keymap.set("n", "<leader>x", require("fzf-lua").quickfix, { silent = true, desc = "🔍Open quickfix" })
+    vim.keymap.set("n", "<leader>fg", require("fzf-lua").live_grep, { silent = true, desc = "🔍Search live grep" })
+    vim.keymap.set("n", "<leader>fb", require("fzf-lua").buffers, { silent = true, desc = "🔍Search buffers" })
+    vim.keymap.set("n", "<leader>fa", function()
+      require("fzf-lua").files({ cwd = "~/ghq" })
+    end, { silent = true, desc = "🔍Search all git files" })
+  end,
 }
