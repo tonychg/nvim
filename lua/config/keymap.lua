91d06b6186f49399f480e4626023955308ea04e2
tonychg <tonychg7@pm.me>
2025-07-08 15:30:26 +0200

    feat: add spectre

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -17,7 +17,7 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
-vim.keymap.set("n", "<leader>cp", ":let @\" = expand('%')<CR>", { desc = "Copy current file path", silent = true })
+vim.keymap.set("n", "<leader>cp", ":let @+ = expand('%')<CR>", { desc = "Copy current file path", silent = true })
 vim.keymap.set("n", "<leader>dl", function()
   vim.lsp.enable(vim.bo.filetype, false)
 end, { desc = "Copy current file path", silent = true })
