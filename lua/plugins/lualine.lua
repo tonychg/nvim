30d872ed36dc2028f6e97ef7889b2fe48b0c62eb
tonychg <tonychg7@pm.me>
2025-07-11 00:13:41 +0200

    fix: remove reference to simplecolors

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require("lualine").setup({
       options = {
-        theme = "simplecolors",
+        theme = "auto",
       },
       sections = {
         lualine_x = { "encoding", "fileformat", "filetype" },
